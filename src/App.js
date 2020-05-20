26e8e4b1aa39d6b49a921905690a90d81740e4d5
Guillermo Alfaro <galfarotolon@gmail.com>
2020-05-20 15:54:45 -0500

    added second  test for episodes component

--- src/App.js
+++ src/App.js
@@ -18,12 +18,15 @@ export default function App() {
 
 
 
+
   useEffect(() => {
+
     fetchShow()
       .then(res => {
         console.log('hi', res.data)
         setShow(res.data)
         setSeasons(formatSeasons(res.data._embedded.episodes))
+
       })
   }, []);
 
---
+++ src/App.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, fireEvent, WaitFor } from '@testing-library/react';
+
+import { fetchShow as mockFetchShow } from './api/fetchShow';
+import userEvent from '@testing-library/user-event'
+import App from './App';
+
+
+const dummyData = {
+    data: [
+        {
+            mission_name: "Thaicom",
+            mission_id: "9D1B7E0",
+            manufacturers: ["Orbital ATK"],
+            payload_ids: ["Thaicom 6", "Thaicom 8"],
+            wikipedia: "https://en.wikipedia.org/wiki/Thaicom",
+            website: "http://www.thaicom.net/en/satellites/overview",
+            twitter: "https://twitter.com/thaicomplc",
+            description:
+                "Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.",
+        },
+        {
+            mission_name: "Telstar",
+            mission_id: "F4F83DE",
+            manufacturers: ["SSL"],
+            payload_ids: ["Telstar 19V", "Telstar 18V"],
+            wikipedia: "https://en.wikipedia.org/wiki/Telesat",
+            website: "https://www.telesat.com/",
+            twitter: null,
+            description:
+                "Telstar 19V (Telstar 19 Vantage) is a communication satellite in the Telstar series of the Canadian satellite communications company Telesat. It was built by Space Systems Loral (MAXAR) and is based on the SSL-1300 bus. As of 26 July 2018, Telstar 19V is the heaviest commercial communications satellite ever launched, weighing at 7,076 kg (15,600 lbs) and surpassing the previous record, set by TerreStar-1 (6,910 kg/15230lbs), launched by Ariane 5ECA on 1 July 2009.",
+        },
+    ],
+};
+
+
+//outside of tests- mock 
+jest.mock('./api/fetchShow')
+
+//test needs to render App, click on the 'get data' button
+//assert that data is rendered - AFTER the api fetch is completed
+
+
+
+test('renders data after API is called', async () => {
+    mockFetchShow.mockResolvedValueOnce(dummyData.data);
+
+    console.log(mockFetchShow.mockResolvedValueOnce(dummyData.data));
+
+    //     userEvent.click(getByText(/get data/i));
+    //     //I want to mimic  even better the events from user interactions
+})
+
